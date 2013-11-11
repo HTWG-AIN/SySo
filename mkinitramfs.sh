b1bde781673a106899a673e8178c4f59aa20b53f
jakub Werner <jack@jaxxnet.de>
2013-11-11 18:10:17 +0100

    added mkinitramfs script

---
+++ mkinitramfs.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/fakeroot /bin/sh
+
+echo "fakeing root enviroment"
+
+echo "creating temp dir"                                                                        
+mkdir tmpmnt
+
+echo "creating dev and nods"
+mkdir tmpmnt/dev
+mknod tmpmnt/dev/console c 5 1 
+mknod tmpmnt/dev/loop0 b 7 0 
+
+echo "copying files"
+cp -r $1 tmpmnt
+
+cd tmpmnt
+find . | cpio -H newc -o > ../initramfs.cpio
+cd ..  
+
+
+echo "xzing initrd"
+xz -z initramfs.cpio
+
+echo "tmpmnt"
+rm -rf tmpmnt
