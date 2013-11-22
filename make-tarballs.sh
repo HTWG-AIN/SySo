b15fd4df64bfb2cc224ff2691673a075683d09bd
Stefano Di Martino <stemarti@homer.springfield>
2013-11-22 13:28:28 +0100

    Lates Tarball

--- make-tarballs.sh
+++ make-tarballs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd uptime-info
-tar czfv uptime-info.tar.gz uptime-info.c && mv uptime-info.tar.gz ../tarballs
+tar czfv uptime-info.tar.gz uptime-info.c Makefile && mv uptime-info.tar.gz ../tarballs
