67a0ba478744e712dd0f2e99f557c21800b8cb0c
Stefano Di Martino <stemarti@homer.springfield>
2013-11-22 09:38:40 +0100

    neues initramfs und copy-to-initramsfs script

---
+++ copy_to_initramfs.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#[ -z $1 ] && echo "usage: $0 <dir_to_be_copied> <initramfs-directory>" && exi
+#[ -z $2 ] && echo "usage: $0 <dir_to_be_copied> <initramfs-directory>" && exi
+
+source=~/git-arm/initramfs
+target=~/buildroot/output/target
+
+echo "copying files from '$source' to '$target'"
+cp -r $source/* $target
