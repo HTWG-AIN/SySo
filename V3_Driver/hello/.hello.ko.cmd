0e87b43a5a38cd0068be803f5b29b7fd2f21ca02
Stefano Di Martino <stemarti@homer.springfield>
2013-12-09 18:49:23 +0100

    bug fix for deleting device nodes

--- V3_Driver/hello/.hello.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/stemarti/git-arm/V3_Driver/hello/hello.ko := ~/buildroot/output/host/usr/bin/arm-linux-ld -r  -T /home/stemarti/buildroot/output/build/linux-3.10.18/scripts/module-common.lds --build-id  -o /home/stemarti/git-arm/V3_Driver/hello/hello.ko /home/stemarti/git-arm/V3_Driver/hello/hello.o /home/stemarti/git-arm/V3_Driver/hello/hello.mod.o
--- V3_Driver/zero/.zero.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/stemarti/git-arm/V3_Driver/zero/zero.ko := ~/buildroot/output/host/usr/bin/arm-linux-ld -r  -T /home/stemarti/buildroot/output/build/linux-3.10.18/scripts/module-common.lds --build-id  -o /home/stemarti/git-arm/V3_Driver/zero/zero.ko /home/stemarti/git-arm/V3_Driver/zero/zero.o /home/stemarti/git-arm/V3_Driver/zero/zero.mod.o
