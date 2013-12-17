77a9f5ddd46efecaab224449de831d8b1b13de02
Stefano Di Martino <stemarti@homer.springfield>
2013-12-17 14:26:24 +0100

    kthread driver fertig

--- V5_Driver/kthread/kthread.c
+++ V5_Driver/kthread/kthread.c
@@ -10,10 +10,9 @@ MODULE_AUTHOR("Stefano Di Martno");
 MODULE_DESCRIPTION("kthread sample");
 MODULE_SUPPORTED_DEVICE("none");
 
-static int thread_id=0;
+static struct task_struct *thread_id;
 static wait_queue_head_t wq;
 static DECLARE_COMPLETION(on_exit);
-static struct task_struct *my_kthread;
 
 static int thread_code(void *data)
 {
@@ -22,68 +21,47 @@ static int thread_code(void *data)
 	//daemonize("MySySoKThread");
 	allow_signal(SIGTERM);
 
-	while (1) 
+	while (kthread_should_stop() == 0) 
 	{
 		timeout = 2 * HZ; // wait 2 second
 		timeout = wait_event_interruptible_timeout(wq, (timeout == 0), timeout);
 
-		pr_info("thread_code: woke up ...\n");
+		pr_debug("thread_code: woke up ...\n");
 
-		if(timeout == -ERESTARTSYS || kthread_should_stop()) 
+		if(timeout == -ERESTARTSYS) 
 		{
-			printk("got signal, break\n");
+			pr_info("got signal, break\n");
 			break;
 		}
 	}
 
-	thread_id = 0;
 	complete_and_exit(&on_exit, 0);
 }
 
 static int __init kthread_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "kthread: Hello, world\n");
 
 	init_waitqueue_head(&wq);
-	// thread_id = kernel_thread(thread_code, NULL, CLONE_KERNEL);
+	thread_id = kthread_create(thread_code, NULL, "MySySoKThread");
 
-	my_kthread = kthread_run(thread_code, NULL, "MySySoKThread");
-
-	if(my_kthread == ERR_PTR(-ENOMEM))
+	if(thread_id == 0)
 	{
 		pr_crit("kthread could not be created!\n");
 		return -EIO;
 	}
+
+	wake_up_process(thread_id);
 	
 	return 0;
 }
 
 static void __exit kthread_exit(void)
 {
-	int ret;
-	printk(KERN_ALERT "Goodbye, cruel world\n");
-
-	//if(thread_id) kill_proc(thread_id, SIGTERM, 1);
+	kill_pid(task_pid(thread_id), SIGTERM, 1);
+	wait_for_completion(&on_exit);
 	
-	if(thread_id)
-	{ 
-		ret = kthread_stop(my_kthread);
-		
-		if(ret == -EINTR)
-		{
-			pr_crit("kthread could not be stoped!\n");
-		}
-		else if (ret < 0)
-		{
-			pr_crit("kthread stoped with error %d!\n", ret);
-		}
-		else
-		{
-			pr_debug("kthread stopped!\n");
-		}
-	}
-	
-	wait_for_completion( &on_exit );
+	printk(KERN_ALERT "kthread: Goodbye, cruel world\n");
 }
 
 module_init(kthread_init);
