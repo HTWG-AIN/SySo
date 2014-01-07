3c5263a87e117eb98ead9d96277e4c9a5c62b21e
Jakub Werner <jawerner@homer.springfield>
2014-01-07 12:34:48 +0100

    read and write

---
+++ initramfs/bin/read1.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+for i in 1 2 3 4 5 6
+do
+  cat /dev/t12buf_threaded 
+  sleep  $(($RANDOM % 6 + 5))
+done
---
+++ initramfs/bin/write.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+for i in 1 2 3 4 5 6
+do
+  echo "Write instanz 0 : Hier ist write $i" > /dev/t12buf_threaded 
+  sleep  $(($RANDOM % 5 + 1))
+done
