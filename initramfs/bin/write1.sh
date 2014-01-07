eeb236c8867917b8b75970e9e901936025bf4be2
Stefano Di Martino <stemarti@homer.springfield>
2014-01-07 23:07:02 +0100

    write skripts: Texte gekürzt

--- initramfs/bin/write1.sh
+++ initramfs/bin/write1.sh
@@ -2,6 +2,6 @@
 
 for i in 1 2 3 4 5 6
 do
-  echo "Write instanz 1 : Hier ist write $i" > /dev/t12buf_threaded 
+  echo "Write instanz 1 : write $i" > /dev/t12buf_threaded 
   sleep  $(($RANDOM % 5 + 1))
 done
