712f5067101d5740da1f48d7b086fc5cf9090350
jakub Werner <jack@jaxxnet.de>
2014-01-13 21:18:02 +0100

    current state not working

--- V6/mygpio/mygpio.c
+++ V6/mygpio/mygpio.c
@@ -10,6 +10,8 @@
 #include <asm/uaccess.h>	// copy_to_user()
 #include <asm/segment.h>
 #include <linux/buffer_head.h>
+#include <linux/gpio.h>
+#include <linux/io.h>
 
 MODULE_AUTHOR("Jakub Werner");
 MODULE_LICENSE("GPL");
@@ -20,20 +22,25 @@ MODULE_SUPPORTED_DEVICE("none");
 #define MAJORNUM 150
 #define NUMDEVICES 1
 
-#define GPFSEL0 0xF2200000
-#define GPFSEL1 0xF2200004
-#define GPFSEL2 0xF2200008
-#define GPFSEL3 0xF220000C
-#define GPFSEL4 0xF2200010
-#define GPFSEL5 0xF2200014
-#define GPSET0  0xF220001c
-#define GPSET1  0xF2200020
-#define GPCLR0  0xF2200028
-#define GPCLR1  0xF220002C
+#define GPFSEL0 0x7E200000
+#define GPFSEL1 0x7E200004
+#define GPFSEL2 0x7E200008
+#define GPFSEL3 0x7E20000C
+#define GPFSEL4 0x7E200010
+#define GPFSEL5 0x7E200014
+#define GPSET0 0x7E20001C
+#define GPSET1 0x7E200020
+#define GPCLR0 0x7E200028
+#define GPCLR1 0x7E20002C
+#define GPLEV0 0x7E200034
+#define GPLEV1 0x7E200038
 
 
-#define GPIO_PORT_18
-#define GPIO_PORT_25
+#define GPIO_PORT_18_SEL
+#define GPIO_PORT_25_SEL
+#define GPIO_PORT_18_SEL
+#define GPIO_PORT_25_SET
+#define GPIO_PORT_25_SET
 
 
 static struct cdev *cdev = NULL;
@@ -63,15 +70,25 @@ static struct file_operations fops = {
 static ssize_t driver_read(struct file *instance, char *user, size_t count,
 			   loff_t * offset)
 {
-    u32 *ptr = (u32 *)GPFSEL1; u32 old_value;  
-    old_value = *ptr;  
-	copy_to_user(user, &old_value, sizeof(old_value));
-	return 0;
+    u32 *ptr_port_button = (u32 *) GPLEV0; 
+    u32 old_value_button = readl(ptr_port_button);
+    rmb();
+
+    printk("Read from port 0x%08x\n", old_value_button);
+
+
+	int not_copied = copy_to_user(user, &old_value_button , sizeof(u32));
+
+
+	return  not_copied;
 }
 
 static ssize_t driver_write(struct file *instance, const char __user * userbuf, size_t count, loff_t * off) 
 {
-    return 0;
+    int userval;
+
+    int not_copied = kstrtoint_from_user(userbuf, count, 0, &userval);
+    return count;
 }
 
 static ssize_t driver_close(struct inode *inode, struct file *file)
@@ -81,6 +98,15 @@ static ssize_t driver_close(struct inode *inode, struct file *file)
 
 static ssize_t driver_open(struct inode *inode, struct file *file)
 {
+    
+    // setze port 25 als eingang
+    u32 *ptr_port_button = (u32 *)GPFSEL2; 
+    u32 old_value_button = readl(ptr_port_button);
+    // clear its fuer port 25 = FFFC7FFF
+    u32 cleared_ports = old_value_button & 0xFFFC7FFF;
+    printk("oldvalue: %08x, cleared-ports: %08x",old_value_button, cleared_ports);
+
+
 	return 0;
 }
 
@@ -117,6 +143,7 @@ static int __init mod_init(void)
 
 	dev_class = class_create(THIS_MODULE, DEVNAME);
 	device_create(dev_class, NULL, major_nummer, NULL, DEVNAME);
+
 
 
 	return 0;
