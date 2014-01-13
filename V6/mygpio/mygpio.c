4a1eeb6f88d23a869fb402acff0cbbff267b2842
Jakub Werner <jawerner@homer.springfield>
2014-01-13 16:16:45 +0100

    mygpio

--- V6/mygpio/mygpio.c
+++ V6/mygpio/mygpio.c
@@ -16,8 +16,28 @@ MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("mygpio device");
 MODULE_SUPPORTED_DEVICE("none");
 
+#define DEVNAME "mygpio"
+#define MAJORNUM 150
+#define NUMDEVICES 1
 
-#define DEVNAME "t12mygpio"
+#define GPFSEL0 0xF2200000
+#define GPFSEL1 0xF2200004
+#define GPFSEL2 0xF2200008
+#define GPFSEL3 0xF220000C
+#define GPFSEL4 0xF2200010
+#define GPFSEL5 0xF2200014
+#define GPSET0  0xF220001c
+#define GPSET1  0xF2200020
+#define GPCLR0  0xF2200028
+#define GPCLR1  0xF220002C
+
+
+#define GPIO_PORT_18
+#define GPIO_PORT_25
+
+
+static struct cdev *cdev = NULL;
+static struct class *dev_class;
 
 
 
@@ -43,12 +63,15 @@ static struct file_operations fops = {
 static ssize_t driver_read(struct file *instance, char *user, size_t count,
 			   loff_t * offset)
 {
+    u32 *ptr = (u32 *)GPFSEL1; u32 old_value;  
+    old_value = *ptr;  
+	copy_to_user(user, &old_value, sizeof(old_value));
 	return 0;
 }
 
 static ssize_t driver_write(struct file *instance, const char __user * userbuf, size_t count, loff_t * off) 
 {
-    return 0
+    return 0;
 }
 
 static ssize_t driver_close(struct inode *inode, struct file *file)
@@ -60,19 +83,24 @@ static ssize_t driver_open(struct inode *inode, struct file *file)
 {
 	return 0;
 }
+
+
 static int __init mod_init(void)
 {
+
 	dev_t major_nummer = MKDEV(MAJORNUM, 0);
 
-	if (register_chrdev_region(MKDEV(MAJORNUM, 0), NUMDEVICES, DEVNAME)) {
-		pr_warn("Device number 0x%x not available ...\n",
-			MKDEV(MAJORNUM, 0));
+	printk(KERN_ALERT "mypgio: Hello, world!\n");
+
+	if (register_chrdev_region(major_nummer, NUMDEVICES, DEVNAME)) {
+		pr_warn("Device number 0x%x not available ...\n", MKDEV(MAJORNUM, 0));
 		return -EIO;
 	}
 
 	pr_info("Device number 0x%x created\n", MKDEV(MAJORNUM, 0));
 
 	cdev = cdev_alloc();
+
 	if (cdev == NULL) {
 		pr_warn("cdev_alloc failed!\n");
 		goto free_devnum;
@@ -87,128 +115,32 @@ static int __init mod_init(void)
 		goto free_cdev;
 	}
 
-	device = device;
-
 	dev_class = class_create(THIS_MODULE, DEVNAME);
-	device = device_create(dev_class, NULL, major_nummer, NULL, DEVNAME);
+	device_create(dev_class, NULL, major_nummer, NULL, DEVNAME);
 
-	// Device Specific operations.
 
 	return 0;
 
- free_cdev:
+      free_cdev:
 	kobject_put(&cdev->kobj);
 	cdev = NULL;
- free_devnum:
+      free_devnum:
 	unregister_chrdev_region(MKDEV(MAJORNUM, 0), NUMDEVICES);
 	return -1;
 }
 
 static void __exit mod_exit(void)
 {
+	printk(KERN_ALERT "mypgio: Goodbye, cruel world\n");
+	device_destroy(dev_class, MKDEV(MAJORNUM, 0));
+	class_destroy(dev_class);
+
 	if (cdev) {
 		cdev_del(cdev);
 	}
 
-	device_destroy(dev_class, MKDEV(MAJORNUM, 0));
-	class_destroy(dev_class);
 	unregister_chrdev_region(MKDEV(MAJORNUM, 0), NUMDEVICES);
-}
 
-static int gpio_set_value(char *port, int value){
-    FILE *gpio_port = NULL;
-    size_t write_count;
-    int fclose_ret = 0;
-    char value_string_path[100];
-    char int_string_value[4];
-    sprintf(int_string_value, "%d", value);
-    sprintf(value_string_path, "/sys/class/gpio/gpio%s/value", port);
-    gpio_port = fopen(value_string_path, "w");
-    if (gpio_port == NULL) {
-        printf("failed get write permission to set port %s's value\n", port);
-        return -1;
-    }
-    write_count = fwrite(&int_string_value, sizeof(char), 1, gpio_port);
-    if (write_count > 0 ) {
-        ;
-    }
-
-    fclose_ret = fclose(gpio_port);
-    if (fclose_ret == EOF) {
-        printf("failed to close value file\n");
-        return -1;
-    }
-
-    return 1;
-}
-
-
-static int gpio_set_direction(char *port, char *direction) {
-    FILE *gpio_port = NULL;
-    size_t write_count;
-    int fclose_ret = 0;
-    char direction_string_path[100];
-    sprintf(direction_string_path, "/sys/class/gpio/gpio%s/direction", port);
-    gpio_port = fopen(direction_string_path, "w");
-    if (gpio_port == NULL) {
-        printf("failed get write permission to set port %s's direction", port);
-        return -1;
-    }
-    write_count = fwrite(direction, sizeof(char), strlen(direction) + 1, gpio_port);
-    if (write_count > 0 ) {
-        ;
-    }
-
-    fclose_ret = fclose(gpio_port);
-    if (fclose_ret == EOF) {
-        return -1;
-    }
-
-    return 1;
-
-}
-static int gpio_export_port(char *port) {
-    FILE *export = NULL;
-    size_t write_count;
-    int fclose_ret = 0;
-    export = fopen("/sys/class/gpio/export", "w");
-    if (export == NULL) {
-        printf("failed get write permission to export port %s", port);
-        return -1;
-    }
-    write_count = fwrite(port, sizeof(char), strlen(port) + 1, export);
-    if (write_count > 0 ) {
-        ;
-    }
-
-    fclose_ret = fclose(export);
-    if (fclose_ret == EOF) {
-        return -1;
-    }
-
-    return 1;
-}
-
-static int gpio_unexport_port(char *port) {
-    FILE *unexport = NULL;
-    size_t write_count;
-    int fclose_ret = 0;
-    unexport = fopen("/sys/class/gpio/unexport", "w");
-    if (unexport == NULL) {
-        printf("failed get write permission to unexport port %s", port);
-        return -1;
-    }
-    write_count = fwrite(port, sizeof(char), strlen(port) + 1, unexport);
-    if (write_count > 0 ) {
-        ;
-    }
-
-    fclose_ret = fclose(unexport);
-    if (fclose_ret == EOF) {
-        return -1;
-    }
-
-    return 1;
 }
 
 module_init(mod_init);
