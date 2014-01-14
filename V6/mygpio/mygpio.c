8220eb3a0592627d9b37f30def84e72676dd4197
Jakub Werner <jawerner@homer.springfield>
2014-01-14 15:20:54 +0100

    my gpio working

--- V6/mygpio/mygpio.c
+++ V6/mygpio/mygpio.c
@@ -12,6 +12,7 @@
 #include <linux/buffer_head.h>
 #include <linux/gpio.h>
 #include <linux/io.h>
+#include <asm/io.h>
 
 MODULE_AUTHOR("Jakub Werner");
 MODULE_LICENSE("GPL");
@@ -22,18 +23,20 @@ MODULE_SUPPORTED_DEVICE("none");
 #define MAJORNUM 150
 #define NUMDEVICES 1
 
-#define GPFSEL0 0x7E200000
-#define GPFSEL1 0x7E200004
-#define GPFSEL2 0x7E200008
-#define GPFSEL3 0x7E20000C
-#define GPFSEL4 0x7E200010
-#define GPFSEL5 0x7E200014
-#define GPSET0 0x7E20001C
-#define GPSET1 0x7E200020
-#define GPCLR0 0x7E200028
-#define GPCLR1 0x7E20002C
-#define GPLEV0 0x7E200034
-#define GPLEV1 0x7E200038
+#define GPBASE(x) (0xF2200000 | x)
+
+#define GPFSEL0 GPBASE(0x0000)
+#define GPFSEL1 GPBASE(0x0004)
+#define GPFSEL2 GPBASE(0x0008)
+#define GPFSEL3 GPBASE(0x000C)
+#define GPFSEL4 GPBASE(0x0010)
+#define GPFSEL5 GPBASE(0x0014)
+#define GPSET0 GPBASE(0x001C)
+#define GPSET1 GPBASE(0x0020)
+#define GPCLR0 GPBASE(0x0028)
+#define GPCLR1 GPBASE(0x002C)
+#define GPLEV0 GPBASE(0x0034)
+#define GPLEV1 GPBASE(0x0038)
 
 
 #define GPIO_PORT_18_SEL
@@ -41,6 +44,19 @@ MODULE_SUPPORTED_DEVICE("none");
 #define GPIO_PORT_18_SEL
 #define GPIO_PORT_25_SET
 #define GPIO_PORT_25_SET
+
+ 
+static int read_bit(u32 n , int bit) {
+    return (n & (1 << bit)) >> bit;
+}
+
+static u32 turn_on_bit(u32 n, int bitnum) {
+        return n | (1 << bitnum);
+}
+
+static u32 turn_off_bit(u32 n, int bitnum) {
+        return n & (~ (1 << bitnum));
+}      
 
 
 static struct cdev *cdev = NULL;
@@ -56,6 +72,7 @@ static int driver_open(struct inode *inode, struct file *instance);
 static ssize_t driver_write(struct file *instance, const char __user * userbuf, size_t count, loff_t * off);
 static int driver_close(struct inode *inode, struct file *instance);
 static ssize_t driver_read(struct file *instance, char *user, size_t count, loff_t * offset);
+static int read_bit(u32 n , int bit);
 
 
 static struct file_operations fops = {
@@ -70,17 +87,19 @@ static struct file_operations fops = {
 static ssize_t driver_read(struct file *instance, char *user, size_t count,
 			   loff_t * offset)
 {
-    u32 *ptr_port_button = (u32 *) GPLEV0; 
+    u32 *ptr_port_button = (u32*) GPLEV0;
     u32 old_value_button = readl(ptr_port_button);
     rmb();
 
-    printk("Read from port 0x%08x\n", old_value_button);
+    char data[5];
+
+    int size = sprintf(data, "%d\n", read_bit(old_value_button, 25));
 
 
-	int not_copied = copy_to_user(user, &old_value_button , sizeof(u32));
+	int not_copied = copy_to_user(user, data , size);
 
 
-	return  not_copied;
+	return  size - not_copied;
 }
 
 static ssize_t driver_write(struct file *instance, const char __user * userbuf, size_t count, loff_t * off) 
@@ -88,8 +107,31 @@ static ssize_t driver_write(struct file *instance, const char __user * userbuf, 
     int userval;
 
     int not_copied = kstrtoint_from_user(userbuf, count, 0, &userval);
-    return count;
+    u32 *ptr_port_led = NULL;
+
+    if (userval == 1) {
+        ptr_port_led = (u32*)GPSET0;
+    }
+    if (userval == 0) {
+        ptr_port_led = (u32*)GPCLR0;
+    }
+    if (ptr_port_led == NULL) {
+        return count;
+    }
+    u32 old_value_led = 0x00000000;
+
+
+    wmb();
+    writel(turn_on_bit(old_value_led, 18), ptr_port_led);
+
+
+    u32 rread = readl(ptr_port_led);
+    printk("Read from port 0x%08x\n", rread);
+
+    
+    return count - not_copied;
 }
+
 
 static ssize_t driver_close(struct inode *inode, struct file *file)
 {
@@ -98,13 +140,32 @@ static ssize_t driver_close(struct inode *inode, struct file *file)
 
 static ssize_t driver_open(struct inode *inode, struct file *file)
 {
-    
-    // setze port 25 als eingang
-    u32 *ptr_port_button = (u32 *)GPFSEL2; 
+
+
+    u32 *ptr_port_button = (u32*)GPFSEL2;
     u32 old_value_button = readl(ptr_port_button);
+    rmb();
     // clear its fuer port 25 = FFFC7FFF
     u32 cleared_ports = old_value_button & 0xFFFC7FFF;
-    printk("oldvalue: %08x, cleared-ports: %08x",old_value_button, cleared_ports);
+
+    // writing 
+    printk("Cleared ports: 0x%08x, ptr-button: %p\n", cleared_ports, ptr_port_button);
+    wmb();
+    writel(cleared_ports, ptr_port_button);
+
+
+
+
+    u32 *ptr_port_led = (u32*)GPFSEL1;
+    u32 old_value_led = readl(ptr_port_led);
+    rmb();
+    // clear its fuer port 18 = F8FFFFFF
+    u32 cleared_ports_led = old_value_led & 0xF8FFFFFF;
+    u32 new_ports_led = cleared_ports_led | 0x01000000;
+    printk("Cleared ports: 0x%08x, new-ports: 0x%08x ptr-led: %p\n", cleared_ports, new_ports_led, ptr_port_led);
+    wmb();
+    writel(new_ports_led, ptr_port_led);
+    
 
 
 	return 0;
@@ -143,6 +204,7 @@ static int __init mod_init(void)
 
 	dev_class = class_create(THIS_MODULE, DEVNAME);
 	device_create(dev_class, NULL, major_nummer, NULL, DEVNAME);
+
 
 
 
